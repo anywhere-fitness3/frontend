eb05c3c342ffc46d547adfe2e79b4420bab2aec7
Tyler <nuvallo.inc@gmail.com>
2020-01-09 22:47:41 -0500

    EditorForm finished

--- anywhere-fitness/src/components/Form/EditorForm.js
+++ anywhere-fitness/src/components/Form/EditorForm.js
@@ -1,68 +1,148 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Container, Button, Form, FormGroup, Label, Input } from "reactstrap";
 import ClassList from "../ClassList";
 
 function EditorForm() {
+  const [classCred, setClassCred] = useState({
+    title: "",
+    time: "",
+    date: "",
+    duration: "",
+    type: "",
+    intensity: "",
+    location: ""
+  });
+
+  const submitForm = event => {
+    event.preventDefault();
+    function validate() {
+      let isError = false;
+      const errors = {
+        titleError: "",
+        timeError: "",
+        dateError: "",
+        durationError: "",
+        typeError: "",
+        intensityError: "",
+        locationError: ""
+      };
+
+      if (classCred.title.length < 1) {
+        isError = true;
+        errors.titleError = "Title Cannot be empty";
+        alert(errors.titleError);
+      }
+
+      if (classCred.time.length < 1) {
+        isError = true;
+        errors.timeError = "Time Cannot be empty";
+        alert(errors.timeError);
+      }
+
+      if (classCred.date.length < 1) {
+        isError = true;
+        errors.dateError = "Date cannot be empty";
+        alert(errors.dateError);
+      }
+
+      if (classCred.duration.length < 1) {
+        isError = true;
+        errors.durationError = "Duration cannot be empty";
+        alert(errors.durationError);
+      }
+      if (classCred.type.length < 1) {
+        isError = true;
+        errors.typeError = "Type of class must be selected";
+        alert(errors.typeError);
+      }
+      if (classCred.intensity.length < 1) {
+        isError = true;
+        errors.intensityError = "Intensity level must be selected";
+        alert(errors.intensityError);
+      }
+      if (classCred.intensity.length < 1) {
+        isError = true;
+        errors.intensityError = "Location cannot be empty";
+        alert(errors.intensityError);
+      }
+
+      return isError;
+    }
+    const err = validate();
+    if (!err) {
+      // clear form
+      setClassCred({
+        title: "",
+        titleError: "",
+        time: "",
+        timeError: "",
+        date: "",
+        dateError: "",
+        duration: "",
+        durationError: "",
+        type: "",
+        typeError: "",
+        intensity: "",
+        intensityError: "",
+        location: "",
+        locationError: ""
+      });
+    }
+
+    console.log(classCred);
+  };
+
+  const handleChanges = event => {
+    setClassCred({ ...classCred, [event.target.name]: event.target.value });
+  };
   return (
     <>
       <Container className="form-container">
         <Form className="form">
           <h1 className="form-heading">Add a New Class</h1>
           <FormGroup>
-            <Label className="form-label" for="email">
+            <Label className="form-label" for="title">
               Title
             </Label>
             <Input
+              onChange={handleChanges}
               type="text"
-              name="email"
-              id="email"
+              name="title"
+              id="title"
               placeholder="Enter Title"
             />
           </FormGroup>
           <FormGroup>
-            <Label className="form-label" for="password">
+            <Label className="form-label" for="time">
               Time
             </Label>
-            <Input
-              type="time"
-              name="password"
-              id="password"
-              placeholder="Enter Password"
-            />
+            <Input onChange={handleChanges} type="time" name="time" id="time" />
           </FormGroup>
           <FormGroup>
-            <Label className="form-label" for="password">
+            <Label className="form-label" for="date">
               Date
             </Label>
-            <Input
-              type="date"
-              name="password"
-              id="password"
-              placeholder="Enter Password"
-            />
+            <Input onChange={handleChanges} type="date" name="date" id="date" />
           </FormGroup>
           <FormGroup>
-            <Label className="form-label" for="password">
+            <Label className="form-label" for="duration">
               Duration
             </Label>
             <Input
+              onChange={handleChanges}
               type="number"
-              name="password"
-              id="password"
+              name="duration"
+              id="duration"
               placeholder="Enter Duration"
             />
           </FormGroup>
           <FormGroup>
-            <Label className="form-label" for="password">
+            <Label className="form-label" for="type">
               Type
             </Label>
-            <Input
-              type="select"
-              name="password"
-              id="password"
-              placeholder="Enter Duration"
-            >
+            <Input onChange={handleChanges} type="select" name="type" id="type">
+              <option disabled>Type of class</option>
               <option>Circuit Training</option>
               <option>Cycling</option>
               <option>Matrial Arts</option>
@@ -71,30 +151,33 @@ function EditorForm() {
             </Input>
           </FormGroup>
           <FormGroup>
-            <Label className="form-label" for="password">
+            <Label className="form-label" for="intensity">
               Intensity
             </Label>
             <Input
+              onChange={handleChanges}
               type="select"
-              name="password"
-              id="password"
-              placeholder="Enter Duration"
+              name="intensity"
+              id="intensity"
             >
               <option>low</option>
+              <option>medium</option>
+              <option>high</option>
             </Input>
           </FormGroup>
           <FormGroup>
-            <Label className="form-label" for="password">
+            <Label className="form-label" for="location">
               Location
             </Label>
             <Input
+              onChange={handleChanges}
               type="text"
-              name="password"
-              id="password"
+              name="location"
+              id="location"
               placeholder="Enter Location"
             />
           </FormGroup>
-          <Button>Add</Button>
+          <Button onClick={submitForm}>Add</Button>
         </Form>
       </Container>
       <ClassList />
